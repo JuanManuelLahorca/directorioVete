dd21bccaa27b63d53c3d2048bddc974c603a588a
JuanManuelLahorca <90331240+JuanManuelLahorca@users.noreply.github.com>
2022-07-29 19:45:22 -0300

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -24,9 +24,9 @@
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'juanmanuellahorca@gmail.com';                     //SMTP username
-            $mail->Password   = 'gpnmgyrfhwzuwfnq';                               //SMTP password
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
             $mail->setFrom('juanmanuellahorca@gmail.com', 'Directorio Veterinario');
@@ -77,9 +77,9 @@
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'juanmanuellahorca@gmail.com';                     //SMTP username
-            $mail->Password   = 'gpnmgyrfhwzuwfnq';                               //SMTP password
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
             $mail->setFrom('juanmanuellahorca@gmail.com', 'Directorio Veterinario');
